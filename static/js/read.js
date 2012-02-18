0e081129ccc8347b497ad8dd45dbf46d52bb0893
Kevin Hwang <k3vinhwang@gmail.com>
2012-02-18 06:22:03 -0800

    beginning of merge

--- static/js/read.js
+++ static/js/read.js
@@ -16,7 +16,7 @@ window.onload = function() {
         $('#magazine').turn('page', page)
     })
 
-    $.get("/book/"+room+"?ch=5", function (data) {
+    $.get("/book/cristo?ch=2", function (data) {
         //console.log(data)
         //console.log(data)
         //console.log(paragraphs)
@@ -29,7 +29,7 @@ window.onload = function() {
         var toReturn = []
         var toAppend = []
         var charCount = 0;
-        var maxCount = 3000;
+        var maxCount = 1750;
 
         for (var i=0;i <= paragraphs.length-1;i++) {
             toAppend.push("<p>")
@@ -38,7 +38,7 @@ window.onload = function() {
             var text = paragraph.innerHTML
             //console.log(text)
             var wordsList = text.split(" ")
-            //console.log(wordsList)
+            console.log(wordsList)
             for (var j=0; j <= wordsList.length-1;j++) {
                 var word = wordsList[j]
                 charCount += word.length + 1
@@ -55,9 +55,10 @@ window.onload = function() {
                 }
             }
             toAppend.push("</p>")
+            charCount += 90
         }
         //console.log(toReturn)
-        $("#magazineholder").append("<div id='magazine'></div>")
+        $("#magazineholder").append("<div id='magazine'></div>");
         for (var i = 0; i <= toReturn.length - 1; i++) {
             var lst = toReturn[i]
             //console.log(lst)
@@ -69,27 +70,29 @@ window.onload = function() {
 
             $("#magazine").append("<div id='page"+i+"'><div id='page"+i+"content' class='page-content'>"+retString+"</div></div>")
             if ((i % 2) == 0) { //this means right hand side, aka odd numbered pages
-                $("#page"+i).css({"background-color" : "#B7B3BA"})
+                $("#page"+i).css({"background-color" : "#B7B3BA"});
             } else {
-                $("#page"+i).css({"background-color" : "#F0F0F0"})
+                $("#page"+i).css({"background-color" : "#F0F0F0"});
             }
+            $("#page"+i+"content").css({"font-size" : "40px"});
             
             retString = ""
         }
 
-        $('#magazine').turn({width:1024, height: 800, page: 1, shadows: true, acceleration: true})
+        $('#magazine').turn({page: 1, shadows: true, acceleration: true});
 
         $('#magazine').bind('turning', function(e,page) {
             centerMagazine(page)
-        })
+        });
 
         $('#magazine').bind('turned', function (e, page, pageObj) {
-            console.log(page)
-            console.log(pageObj)
-        })
-    })
+            //console.log(page);
+            //console.log(pageObj);
+        });
+    });
 
     var centerMagazine = function (page) {
+
         var that = $('#magazine');
         var width = that.width();
         var pageWidth = width/2;
@@ -97,7 +100,7 @@ window.onload = function() {
         var leftc = ($(window).width()-width)/2;
         var leftr = ($(window).width()-pageWidth)/2;
         var leftd = (page == 1)? leftr - leftc - pageWidth : leftr - leftc;
-        console.log("INITIALIZE");
+        //console.log("INITIALIZE");
         
         if (page == total) {
             console.log("SHIFT TO END")
@@ -105,17 +108,16 @@ window.onload = function() {
         }
 
         if (page == 1) {
-            console.log("SHIFT TO FRONT")
-            console.log(leftd)
+            //console.log("SHIFT TO FRONT")
+            //console.log(leftd)
             $('#magazine').animate({left: 0 });
 
         } else if (page == 2) {
-            console.log("SHIFT FROM 1")
-            console.log(leftd)
+            //console.log("SHIFT FROM 1")
+            //console.log(leftd)
             $('#magazine').animate({left: leftd});
         } else if (page == total - 1) {
             $('#magazine').animate({left: leftd});
-        } 
+        }
     }
-
 }
