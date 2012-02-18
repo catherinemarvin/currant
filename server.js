95456329410ea23b127779cf33a2090bf0d8d010
Kevin Hwang <k3vinhwang@gmail.com>
2012-02-18 10:47:41 -0800

    tokbox

--- server.js
+++ server.js
@@ -76,6 +76,8 @@ server.post('/upload/:id', function(req, res) {
 var roomPages = {} // a dictionary of current page numbers, indexed by room
 var userCounts = {} // a dictionary of current counts of users, indexed by room
 
+var roomVideos = {} //dictionary of streams, indexed by room
+
 io.sockets.on('connection', function(socket) {
 
     socket.on('setRoomAndUser', function(data) {
@@ -106,6 +108,35 @@ io.sockets.on('connection', function(socket) {
                 delete userCounts[room]
             }
         })
-    })
+    });
+
+    socket.on('createdStream', function (roomAndStream) { //JSON with room and id
+        console.log("MY CREATION")
+        var room = roomAndStream.room
+        var stream = roomAndStream.stream
+        var myId = roomAndStream.myId
+        console.log(myId)
+        if (room in roomVideos) {
+            if (roomVideos[room].indexOf(stream) == -1) {
+                roomVideos[room].push(stream)
+            }
+        } else {
+            roomVideos[room] = [stream]
+        }
+        socket.emit('subscribe', {"myid" : myId, "streams" : roomVideos[room]})
+    });
+
+    socket.on('subscribeToStream', function (roomAndId) {
+        var room = roomAndId.room
+        var id = roomAndId.id
+        console.log("subscribing")
+        if (room in roomVideos) {
+            console.log("already exists")
+            console.log(roomVideos[room])
+            socket.emit('subscribe',{"myid" : id, "streams" : roomVideos[room]})
+        } else {
+            console.log("nothing")
+        }
+    });
 
 })
