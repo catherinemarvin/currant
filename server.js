7a7cf07add9a636ddbb64e7a447c934d89297cf5
Ross Breytberg <rossbreytberg@gmail.com>
2012-02-17 20:13:11 -0800

    server save uploaded posts

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var io = require('socket.io')
+var fs = require('fs')
 
 var server = express.createServer()
 server.set('view engine', 'ejs')
@@ -7,8 +8,35 @@ server.set('view options', {
     layout: false
 })
 server.set('views', __dirname + "/views")
+server.set('tmp', __dirname + "/tmp")
 server.use("/static", express.static(__dirname + "/static"))
 server.listen(80)
 
 var io = io.listen(server)
 io.set('log level', 1)
+
+
+
+server.get('/', function(req, res) {
+	res.render('index', {})
+})
+
+server.post('/upload/:id', function(req, res) {
+	var form = new formidable.IncomingForm();
+            form.parse(req))
+            form.onPart = function(part) {
+                part.on('data', function(data) {
+                    fs.open('tmp/'+req.params.id, 'w', function(err, fd) {
+						fs.write(fd, data, undefined, undefined, function(err, written) {
+							console.log('written '+written+' bytes to file '+req.params.id)
+							fs.close(fd)
+						})
+					})
+                })
+                part.on('end', function() {
+                    res.write("File received.")
+                    res.end()
+                })
+            }
+
+})
